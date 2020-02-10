3fd3f4b2a6ddc22f950ed01d9221231d99fca272
LLoyd Headbush <ocanica@gmail.com>
2020-02-10 21:13:30 +0000

    updated Persistent Bugger comment

--- Persistent Bugger.cs
+++ Persistent Bugger.cs
@@ -7,6 +7,7 @@ namespace Disemvowel_Trolls
     public class Persistent_Bugger
     {
         // Unable to complete challenge. Unable to track the number of recursive calls. Exit condition is too obtuse.
+        // Can potentially call the recursive function within the foreach loop and use a counter variable to track the number of calls
         public static int Persistence(long n)
         {
             var sum = 1;
