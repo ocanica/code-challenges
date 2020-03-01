6e44c024534eca97bbbba4605f24d8566c748880
Lloyd Headbush <ocanica@gmail.com>
2020-03-01 08:03:27 +0000

    Added new Kyu 8 Kata

---
+++ FirstNonConsecutive.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static object FirstNonConsecutive(int[] arr)
+            {
+                for (int i = 0; i < arr.Length-1; i++)
+                {
+                    if (arr[i + 1] != arr[i] + 1)
+                        return arr[i + 1];
+                }
+                return null;
+            }
+        }
+    }
+}
